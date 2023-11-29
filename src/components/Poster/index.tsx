d32ad3f5c63dcc8c227a30a928f62aa06516605c
TahereGmi <mstgholami@gmail.com>
2023-11-29 13:55:56 +0330

    Add placeholder image for not having img path

--- src/components/Poster/index.tsx
+++ src/components/Poster/index.tsx
@@ -1,14 +1,15 @@
 import React, { FC } from 'react';
+import notFoundImage from '../../../src/assets/images/not-found-500X750-optimized.jpeg';
 
 type TPosterProps ={
-    path: string
+    path?: string
 }
 
 const Poster: FC<TPosterProps> = ({ path }) => {
     return (
         <img 
             className="center-block" 
-            src={`https://image.tmdb.org/t/p/w500/${path}`} 
+            src={`${path ? `https://image.tmdb.org/t/p/w500/${path}` : notFoundImage}`} 
             alt="Movie poster" 
         />
     );
